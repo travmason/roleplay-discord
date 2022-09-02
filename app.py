24e0048eaf376845006a43dfe22d9f106d9fd70c
Trav mason <travis.mason@gmail.com>
2022-09-02 10:50:20 +1000

    logging with discord.py figured out but work to be done

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
 # bot.py
 import os
 import logging
+import logging.handlers
 import discord
 from dotenv import load_dotenv
 import random
 from website.daniel import Bot  
 
 load_dotenv()
-
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-logging.basicConfig(filename='discord.log', level=logging.INFO)
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+logging.getLogger('discord.http').setLevel(logging.WARNING)
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+consolehandler = logging.StreamHandler()
+consolehandler.setFormatter(formatter)
+logger.addHandler(consolehandler)
+
+logger.info('logging started')
 
 client = discord.Client(intents=discord.Intents.default())
 bot = Bot()
@@ -39,4 +58,4 @@ async def on_message(message):
 
     await message.channel.send(response)
 
-client.run(DISCORD_TOKEN)
+client.run(DISCORD_TOKEN, log_handler=None)
